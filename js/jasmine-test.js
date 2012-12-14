8acfe89a967c66c7d61fec4b12959a5287de1507
AbraaoAlves <abraao.teodosio@gmail.com>
2012-12-14 15:27:02 -0300

    arquivos para debug

---
+++ js/jasmine-test.js
@@ -0,0 +1,16 @@
+///<reference path="knockout-2.2.0.js" />
+///<reference path="ko-es5.js" />
+
+describe("describe", function () {
+    it('should define an enumerable property', function () {
+        var foo = {};
+
+        ko.utils.defineObservableProperty(foo, 'name');
+
+        var descriptor = Object.getOwnPropertyDescriptor(foo, 'name');
+        expect(descriptor.enumerable).toBeTruthy();
+        expect(descriptor.configurable).toBeTruthy();
+        expect(descriptor.get instanceof Function).toBeTruthy();
+        expect(descriptor.set instanceof Function).toBeTruthy();
+    });
+});
